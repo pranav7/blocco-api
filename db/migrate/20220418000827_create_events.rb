1f52dec736b4911c587e71f5db67aa04199cd27e
Pranav Singh <pranav.singh@intercom.io>
2022-04-19 13:10:43 +0100

    Add recurring events

--- db/migrate/20220418000827_create_events.rb
+++ db/migrate/20220418000827_create_events.rb
@@ -4,21 +4,19 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.string :title
       t.datetime :start
       t.datetime :end
-      t.string :start_time
-      t.string :end_time
-      t.datetime :start_recur
-      t.datetime :end_recur
-      t.string :days_of_week
       t.integer :event_type, default: 0
-      t.integer :event_category, default: 0
+      t.integer :status, default: 0
       t.boolean :editable, default: true
       t.boolean :all_day, default: false
       t.string :color
       t.string :background_color
       t.string :border_color
       t.string :text_color
+      t.string :google_event_id
 
       t.timestamps
+
+      t.index :google_event_id, unique: true
     end
   end
 end
